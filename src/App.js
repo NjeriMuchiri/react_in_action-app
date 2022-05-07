894c51bb1d63005491622044329d6372269b54cd
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-05-07 12:20:44 +0300

    Learning to work with react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Learning from './Components/Learning'
+import Ptypes from './Components/Ptypes'
 
 const App = () => {
   return (
     <div>
       <Learning />
+      <Ptypes />
     </div>
   )
 }
--- src/Components/Learning.js
+++ src/Components/Learning.js
@@ -2,6 +2,7 @@ import React from "react";
 import {render} from'react-dom';
 
 const Learning = () =>{
+    
 const node = document.getElementById('root');
 const root = React.createElement('div', {},
  React.createElement('h1', {}, "Hello,World!",
